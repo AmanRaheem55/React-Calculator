1455ce1106df4b089f49ba7ee2f190cd46abfc3a
AmanRaheem <aman.reheem55@gmail.com>
2020-11-02 22:34:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [display, setDisplay] = useState("")
+  const calculat = () =>{
+   const resulls = eval(display)
+    setDisplay(resulls)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="text" value={display} onChange={e => setDisplay(e.target.value)}/>
+        <button onClick={e => {calculat()}}> =</button>
     </div>
   );
 }
