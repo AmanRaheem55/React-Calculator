eefff4bac40dc0b8384b244ddf47f3fff6c561c5
AmanRaheem <aman.reheem55@gmail.com>
2020-11-03 22:39:33 +0530

    adding Styles

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import React, { useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import KeyPad from "./Components/KeyPad";
+import "./App.css";
 
 function App() {
+  const [display, setDisplay] = useState("");
 
-  const [display, setDisplay] = useState("")
+  const calculate = () => {
+    let resulls  =  String(eval(display));
+    console.log(resulls)
+    setDisplay(resulls);
+  };
 
-
-  const calculate = () =>{
-   const resulls = eval(display)
-    setDisplay(resulls)
-  }
-
-  const displayData= (data) => {
-    setDisplay(display + data)
-  }
+  const displayData = (data) => {
+    setDisplay(display + data);
+  };
 
   return (
-    <div className="App">
-        <input type="text" value={display} onChange={e => setDisplay(e.target.value)}/>
-        <button onClick={e => {calculate()}}> =</button>
+    <div className="App   card" >
+  
+      <div class="card-body">
+          <input
+        className="display"
+        type="text"
+        value={display}
+        onChange={(e) => setDisplay(e.target.value)}
+      />
+     
+          <KeyPad className=""
+          displayData={displayData}
+          calculate={calculate}
+        /> 
+       
+      </div>
 
-
-
+      {/* <input   className="" type="text" value={display} onChange={e => setDisplay(e.target.value)}/>
+        <KeyPad className=""
+          displayData={displayData}
+          calculate={calculate}
+        /> */}
     </div>
-    
   );
 }
 
