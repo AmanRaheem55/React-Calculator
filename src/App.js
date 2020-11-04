5baeba37bd8a0a661657d127f7b2c56f9e329a52
AmanRaheem <aman.reheem55@gmail.com>
2020-11-05 02:39:31 +0530

    adding click Envent

--- src/App.js
+++ src/App.js
@@ -1,49 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, } from "react";
 import KeyPad from "./Components/KeyPad";
 import "./App.css";
 
 function App() {
   const [display, setDisplay] = useState("");
 
-  const calculate = () => {
-    let resulls = String(eval(display));
-    console.log(resulls)
-    setDisplay(resulls);
-  };
+     
 
   const displayData = (data) => {
     setDisplay(display + data);
   };
 
-const clearDisplay = () => {
-  setDisplay("")
-}
+    const calculate = () => {
+    let data = display
+    let resulls = eval(data)
+    setDisplay(resulls);
+  };
 
-const backSpace = () =>  {
-  let data = display
+
+
+  const backSpace = () => {
+    let data = display
     let result = data.slice(0, -1)
-      setDisplay(result)
-  
-   
-}
+    setDisplay(result)
+  }
+
+  const clearDisplay = () => {
+    setDisplay("")
+  }
+
+
+
+  const handleClickEvent = (event) => {
+   const {value}= event.target
+   value === "C"  ? clearDisplay() : value === "Ac" ? backSpace() : value === "="  ? calculate() :  displayData(value) 
+  }
+
+
   return (
-    <div className="App btn-group-vertical  " role="group"  >
+    <div className="App btn-group-vertical   container-fluid " role="group"  >
+      <input
+        className="display text-center form-control"
+        type="text"
+        value={display}
+        onChange={(e) => setDisplay(e.target.value)}
+      />
+      <KeyPad className=""
+        handleClickEvent={handleClickEvent}
+      />
 
-        <input
-          className="display text-center form-control-xl"
-          type="text"
-          value={display}
-          onChange={(e) => setDisplay(e.target.value)}
-        />
 
-        <KeyPad className=""
-          displayData={displayData}
-          calculate={calculate}
-          backSpace={backSpace}
-          clearDisplay={clearDisplay}
-        />
-
-      </div>
+    </div>
 
   );
 }
--- src/Components/KeyPad.js
+++ src/Components/KeyPad.js
@@ -1,46 +1,48 @@
 import React from 'react'
 import "../Assets/keyPad.css"
 
-function KeyPad({ calculate, displayData, backSpace }) {
+function KeyPad({handleClickEvent}) {
+
+
     return (
-        <div className="keyPad" >
+        <div className="keyPad " >
         
         <div className="btn-group">
-                <button id="equl" onClick={calculate} className="btn btn-primary" value="=">=</button>
-                <button onClick={backSpace} id="Ac" className="btn btn-warning" value="Ac">Ac</button>
-                <button id="C" className="btn btn-danger" value="C">C</button>
+                <button id="equl" onClick={handleClickEvent} className=" keyPad__key btn btn-primary" value="=">=</button>
+                <button onClick={handleClickEvent} id="Ac"   className=" keyPad__key btn btn-warning" value="Ac">Ac</button>
+                <button onClick={handleClickEvent} id="C" className=" keyPad__key btn btn-danger"  value="C">C</button>
             </div>
             
             <div className="btn-group">
-                <button onClick={e => (displayData(e.target.value))} id="num-1" className="btn btn-dark" value="1">1</button>
-                <button onClick={e => (displayData(e.target.value))} id="num-2" className="btn btn-dark" value="2">2</button>
-                <button onClick={e => (displayData(e.target.value))} id="num-3" className="btn btn-dark" value="3">3</button>
+                <button onClick={handleClickEvent} id="num-1" className=" keyPad__key btn btn-dark" value="1">1</button>
+                <button onClick={handleClickEvent} id="num-2" className=" keyPad__key btn btn-dark" value="2">2</button>
+                <button onClick={handleClickEvent} id="num-3" className=" keyPad__key btn btn-dark" value="3">3</button>
 
             </div>
 
             <div className="btn-group">
-                <button onClick={e => (displayData(e.target.value))} id="num-4" className="btn btn-dark" value="4">4</button>
-                <button onClick={e => (displayData(e.target.value))} id="num-5" className="btn btn-dark" value="5">5</button>
-                <button onClick={e => (displayData(e.target.value))} id="num-6" className="btn btn-dark" value="6">6</button>
+                <button onClick={handleClickEvent} id="num-4" className=" keyPad__key btn btn-dark" value="4">4</button>
+                <button onClick={handleClickEvent} id="num-5" className=" keyPad__key btn btn-dark" value="5">5</button>
+                <button onClick={handleClickEvent} id="num-6" className=" keyPad__key btn btn-dark" value="6">6</button>
 
             </div>
 
             <div className="btn-group ">
-                <button onClick={e => (displayData(e.target.value))} id="num-7" className="btn btn-dark" value="7">7</button>
-                <button onClick={e => (displayData(e.target.value))} id="num-8" className="btn btn-dark" value="8">8</button>
-                <button onClick={e => (displayData(e.target.value))} id="num-9" className="btn btn-dark" value="9">9</button>
+                <button onClick={handleClickEvent} id="num-7" className=" keyPad__key btn btn-dark" value="7">7</button>
+                <button onClick={handleClickEvent} id="num-8" className=" keyPad__key btn btn-dark" value="8">8</button>
+                <button onClick={handleClickEvent} id="num-9" className=" keyPad__key btn btn-dark" value="9">9</button>
             </div>
 
             <div className="btn-group">
-                <button onClick={e => (displayData(e.target.value))} id="add" className="btn btn-dark" value="+">+</button>
-                <button onClick={e => (displayData(e.target.value))} id="num-0" className="btn btn-dark" value="0">0</button>
-                <button onClick={e => (displayData(e.target.value))} id="sub" className="btn btn-dark" value="-">-</button>
+                <button onClick={handleClickEvent} id="add" className=" keyPad__key btn btn-dark" value="+">+</button>
+                <button onClick={handleClickEvent} id="num-0" className=" keyPad__key btn btn-dark" value="0">0</button>
+                <button onClick={handleClickEvent} id="sub" className=" keyPad__key btn btn-dark" value="-">-</button>
             </div>
 
             <div className="btn-group">
-                <button onClick={e => (displayData(e.target.value))} id="div" className="btn btn-dark" value="/">/</button>
-                <button onClick={e => (displayData(e.target.value))} id="C" className="btn btn-dark " value="%">%</button>
-                <button onClick={e => (displayData(e.target.value))} id="mult" className="btn btn-dark" value="*">*</button>
+                <button onClick={handleClickEvent} id="div" className="keyPad__key btn btn-dark" value="/">/</button>
+                <button onClick={handleClickEvent} id="C" className= "keyPad__key btn btn-dark" value="%">%</button>
+                <button onClick={handleClickEvent} id="mult" className=" keyPad__key btn btn-dark" value="*">*</button>
             </div>
 
             
