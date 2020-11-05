90affb8b8b753f30520545a4ce6587d7b2ccfead
AmanRaheem <aman.reheem55@gmail.com>
2020-11-05 11:34:42 +0530

    Adding keyPress event

--- src/App.js
+++ src/App.js
@@ -5,53 +5,46 @@ import "./App.css";
 function App() {
   const [display, setDisplay] = useState("");
 
-     
-
+  const handleKeyPress = (e) => {
+    const data = e.target.value
+      let filteredData = data.replace(/[a-z @ ! # $  ^ & ( ) ~ ` " " : ;]/g, '');
+    setDisplay(filteredData)
+  }
   const displayData = (data) => {
     setDisplay(display + data);
   };
 
-    const calculate = () => {
-    let data = display
-    let resulls = eval(data)
+  const calculate = () => {
+    let data = display;
+    let resulls = eval(data);
     setDisplay(resulls);
   };
 
-
-
   const backSpace = () => {
-    let data = display
-    let result = data.slice(0, -1)
-    setDisplay(result)
-  }
+    let data = display;
+    let result = data.slice(0, -1);
+    setDisplay(result);
+  };
 
   const clearDisplay = () => {
-    setDisplay("")
-  }
-
-
+    setDisplay("");
+  };
 
   const handleClickEvent = (event) => {
-   const {value}= event.target
-   value === "C"  ? clearDisplay() : value === "Ac" ? backSpace() : value === "="  ? calculate() :  displayData(value) 
-  }
-
+    const { value } = event.target;
+    value === "C" ? clearDisplay():value === "Ac" ? backSpace() : value === "=" ? calculate() : displayData(value);
+  };
 
   return (
-    <div className="App btn-group-vertical   container-fluid " role="group"  >
+    <div className="App btn-group-vertical container-fluid "role="toolbar" aria-label="Toolbar with button groups">
       <input
         className="display text-center form-control"
         type="text"
         value={display}
-        onChange={(e) => setDisplay(e.target.value)}
+        onChange={handleKeyPress}
       />
-      <KeyPad className=""
-        handleClickEvent={handleClickEvent}
-      />
-
-
+      <KeyPad className="" handleClickEvent={handleClickEvent} />    
     </div>
-
   );
 }
 
