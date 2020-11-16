24de83d2ce64896cbc4dca4ad6a4010ec7f413ed
Aman <aman.reheem55@gmail.com>
2020-11-16 13:08:55 +0530

    fixibg keypress Event

--- src/App.js
+++ src/App.js
@@ -2,30 +2,42 @@ import React, { useState, useEffect } from "react";
 import KeyPad from "./Components/KeyPad";
 import "./Assets/App.css";
 import "./firebaseConfig";
-
-
 import Display from "./Components/Display";
+
 function App() {
 
+
+
   const handleKeyPress = event => {
-    const { key, } = event;
-    setDisplay(display => display + key)
+    console.log(event)
+    const { key, keyCode } = event;
+    console.log("pressed" + keyCode)
+    let filteredData = key.replace(/[a-z @ ! # $ E B^ & ( ) ~ ` " " : ; > <. ? |]/g, "");
+    keyCode === 13 ? calculate() : displayData(filteredData)
   };
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyPress);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyPress);
-    };
-  }, []);
-
   const [display, setDisplay] = useState("");
 
 
-  const displayData = (data) => {
-    setDisplay(display + data);
+  useEffect(() => {
+    window.addEventListener('keypress', handleKeyPress);
+    return () => {
+      window.removeEventListener('keypress', handleKeyPress);
+    };
+  });
+
+
+  const handleClickEvent = (event) => {
+    const { value } = event.target;
+    value === "C" ? clearDisplay() : value === "Ac" ? backSpace() : value === "=" ? calculate() : displayData(value);
   };
+
+
+  const displayData = (data) => {
+    setDisplay(display => display + data);
+  };
+
+
+
 
   const calculate = () => {
     let data = display;
@@ -40,10 +52,7 @@ function App() {
   };
   const clearDisplay = () => setDisplay("");
 
-  const handleClickEvent = (event) => {
-    const { value } = event.target;
-    value === "C" ? clearDisplay():value === "Ac" ? backSpace() :value === "=" ? calculate():displayData(value);
-  };
+
 
 
 
@@ -56,6 +65,7 @@ function App() {
             className="App__textInput"
             calculate={calculate}
             display={display}
+           
           />
         </div>
         <KeyPad className="App__keypad" handleClickEvent={handleClickEvent} />
--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,13 +1,15 @@
 import React from "react";
 import '../Assets/Display.css'
 
-function Display({ display, }) {
+function Display({ display,calculate }) {
   return (
     <div className="display">
       <input
         className="display__textInput"
         type="text"
-        value={display}
+        value={display} 
+       
+        onKeyPress={(e) => (e.charCode === 13 ? calculate() : null)}
       />
     </div>
   );
